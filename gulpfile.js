d53bb55c92955b949e3f0e2af46117c6690099d7
manuelContrerasDev <manuel.contrerasdev@gmail.com>
2025-01-06 17:41:23 -0300

    sin webpack

--- gulpfile.js
+++ gulpfile.js
@@ -8,65 +8,54 @@ const cssnano = require('cssnano');
 const postcss = require('gulp-postcss');
 const sourcemaps = require('gulp-sourcemaps');
 
-// Webpack
-const webpack = require('webpack-stream');
-const path = require('path');
-
 // Imagenes
 const cache = require('gulp-cache');
 const imagemin = require('gulp-imagemin');
 const webp = require('gulp-webp');
 const avif = require('gulp-avif');
 
-// JavaScript
+// Javascript
 const terser = require('gulp-terser-js');
 const concat = require('gulp-concat');
 const rename = require('gulp-rename');
 
+// Rutas
 const paths = {
     scss: 'src/scss/**/*.scss',
     js: 'src/js/**/*.js',
     imagenes: 'src/img/**/*'
-};
+}
 
-// Tarea CSS
+// Tarea para compilar Sass a CSS
 function css() {
     return src(paths.scss)
         .pipe(sourcemaps.init())
-        .pipe(sass({outputStyle: 'expanded'}))
+        .pipe(sass({ outputStyle: 'expanded' }))
         .pipe(postcss([autoprefixer()]))
-        // .pipe(postcss([autoprefixer(), cssnano()])) // Usa esto para minificar
+        // .pipe(postcss([autoprefixer(), cssnano()]))
         .pipe(sourcemaps.write('.'))
         .pipe(dest('public/build/css'));
 }
 
-// Tarea JavaScript
+// Tarea para procesar JavaScript
 function javascript() {
     return src(paths.js)
-        .pipe(webpack({
-            entry: './src/js/app.js', // Configura el archivo de entrada
-            output: {
-                filename: 'bundle.js', // Configura el archivo de salida
-                path: path.resolve(__dirname, 'public/build/js'), // Ruta donde se guardará el bundle
-            },
-            mode: 'production', // Configura el modo de producción
-            watch: false, // Desactiva el watch para producción
-        }))
         .pipe(sourcemaps.init())
-        .pipe(terser()) // Minifica el código JS
+        .pipe(concat('app.js'))  // Opcional, si necesitas concatenar
+        .pipe(terser())  // Minificación del JS
         .pipe(sourcemaps.write('.'))
-        .pipe(rename({ suffix: '.min' })) // Renombra el archivo con el sufijo .min
-        .pipe(dest('public/build/js')); // Guarda el archivo compilado
+        .pipe(rename({ suffix: '.min' }))  // Renombrar para JS minificado
+        .pipe(dest('public/build/js'));
 }
 
-// Tarea de imágenes
+// Tarea para optimizar imágenes
 function imagenes() {
     return src(paths.imagenes)
         .pipe(cache(imagemin({ optimizationLevel: 3 })))
         .pipe(dest('public/build/img'));
 }
 
-// Tareas de WebP y AVIF
+// Tarea para convertir imágenes a formato WebP
 function versionWebp(done) {
     const opciones = { quality: 50 };
     src('src/img/**/*.{png,jpg}')
@@ -75,6 +64,7 @@ function versionWebp(done) {
     done();
 }
 
+// Tarea para convertir imágenes a formato AVIF
 function versionAvif(done) {
     const opciones = { quality: 50 };
     src('src/img/**/*.{png,jpg}')
@@ -83,7 +73,7 @@ function versionAvif(done) {
     done();
 }
 
-// Tarea de desarrollo (watch)
+// Tarea de desarrollo con watch
 function dev(done) {
     watch(paths.scss, css);
     watch(paths.js, javascript);
@@ -93,11 +83,12 @@ function dev(done) {
     done();
 }
 
-// Exportación de tareas
-module.exports.css = css;
-module.exports.js = javascript;
-module.exports.imagenes = imagenes;
-module.exports.versionWebp = versionWebp;
-module.exports.versionAvif = versionAvif;
-module.exports.dev = parallel(css, imagenes, versionWebp, versionAvif, javascript, dev);
-module.exports.build = parallel(javascript, css, imagenes); // Compilación final
+// Exportar tareas
+exports.css = css;
+exports.js = javascript;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.dev = parallel(css, imagenes, versionWebp, versionAvif, javascript, dev);
+exports.build = parallel(javascript, css, imagenes);
+
