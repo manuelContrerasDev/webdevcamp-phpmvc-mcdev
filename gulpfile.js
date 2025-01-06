6c0507878b55997e5be319c48cf23c51e984b95d
manuelContrerasDev <manuel.contrerasdev@gmail.com>
2025-01-06 15:46:46 -0300

    Corrige la sintaxis de export en gulpfile.js para compatibilidad con Netlify

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const { src, dest, watch, parallel } = require('gulp');
+const { src, dest, watch, parallel } = require('gulp');
 
 // CSS
 const sass = require('gulp-sass')(require('sass'));
@@ -20,25 +20,27 @@ const concat = require('gulp-concat');
 const rename = require('gulp-rename')
 
 // Webpack
-const webpack = require('webpack-stream')
+const webpack = require('webpack-stream');
 
 const paths = {
     scss: 'src/scss/**/*.scss',
     js: 'src/js/**/*.js',
     imagenes: 'src/img/**/*'
-}
+};
+
 function css() {
     return src(paths.scss)
-        .pipe( sourcemaps.init())
-        .pipe( sass({outputStyle: 'expanded'}))
-        .pipe( postcss([autoprefixer()]))
-        // .pipe( postcss([autoprefixer(), cssnano()]))
-        .pipe( sourcemaps.write('.'))
-        .pipe(  dest('public/build/css') );
+        .pipe(sourcemaps.init())
+        .pipe(sass({outputStyle: 'expanded'}))
+        .pipe(postcss([autoprefixer()]))
+        // .pipe(postcss([autoprefixer(), cssnano()]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest('public/build/css'));
 }
+
 function javascript() {
     return src(paths.js)
-        .pipe( webpack({
+        .pipe(webpack({
             module: {
                 rules: [
                     {
@@ -56,48 +58,48 @@ function javascript() {
         .pipe(terser())
         .pipe(sourcemaps.write('.'))
         .pipe(rename({ suffix: '.min' }))
-        .pipe(dest('./public/build/js'))
+        .pipe(dest('./public/build/js'));
 }
 
 function imagenes() {
     return src(paths.imagenes)
-        .pipe( cache(imagemin({ optimizationLevel: 3})))
-        .pipe( dest('public/build/img'))
+        .pipe(cache(imagemin({ optimizationLevel: 3 })))
+        .pipe(dest('public/build/img'));
 }
 
-function versionWebp( done ) {
+function versionWebp(done) {
     const opciones = {
         quality: 50
     };
     src('src/img/**/*.{png,jpg}')
-        .pipe( webp(opciones) )
-        .pipe( dest('public/build/img') )
+        .pipe(webp(opciones))
+        .pipe(dest('public/build/img'));
     done();
 }
 
-function versionAvif( done ) {
+function versionAvif(done) {
     const opciones = {
         quality: 50
     };
     src('src/img/**/*.{png,jpg}')
-        .pipe( avif(opciones) )
-        .pipe( dest('public/build/img') )
+        .pipe(avif(opciones))
+        .pipe(dest('public/build/img'));
     done();
 }
 
 function dev(done) {
-    watch( paths.scss, css );
-    watch( paths.js, javascript );
-    watch( paths.imagenes, imagenes)
-    watch( paths.imagenes, versionWebp)
-    watch( paths.imagenes, versionAvif)
-    done()
+    watch(paths.scss, css);
+    watch(paths.js, javascript);
+    watch(paths.imagenes, imagenes);
+    watch(paths.imagenes, versionWebp);
+    watch(paths.imagenes, versionAvif);
+    done();
 }
 
-exports.css = css;
-exports.js = javascript;
-exports.imagenes = imagenes;
-exports.versionWebp = versionWebp;
-exports.versionAvif = versionAvif;
-exports.dev = parallel( css, imagenes, versionWebp, versionAvif, javascript, dev) ;
-export const build = parallel( js, css, imagenes);
+module.exports.css = css;
+module.exports.js = javascript;
+module.exports.imagenes = imagenes;
+module.exports.versionWebp = versionWebp;
+module.exports.versionAvif = versionAvif;
+module.exports.dev = parallel(css, imagenes, versionWebp, versionAvif, javascript, dev);
+module.exports.build = parallel(javascript, css, imagenes);
